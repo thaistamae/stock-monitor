8061b38f84afb0a8da27ae05e9d4e53c67f6328c
Thais Tamae <thais.tamae@gmail.com>
2022-04-13 16:41:54 -0300

    StockDetails structure

---
+++ src/pages/StockDetails/StockDetails.js
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export function StockDetails() {
+  const { stockSymbol } = useParams();
+  const [stockList, setStockList] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "https://yh-finance.p.rapidapi.com/market/v2/get-quotes",
+    params: { region: "US", symbols: "AAPL,BUD,DIS,MSFT,NKE" },
+    headers: {
+      "X-RapidAPI-Host": "yh-finance.p.rapidapi.com",
+      "X-RapidAPI-Key": "",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((response) => {
+        setStockList(response.data.quoteResponse.result);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  const filteredStock = stockList.filter((stock) => {
+    return stock.symbol === `${stockSymbol}`;
+  });
+
+  console.log(filteredStock);
+
+  return <></>;
+}
